02bbbcdc417edaa62d714c297eeb53e474711b8c
seun <seun.edagolota@gmail.com>
2025-10-07 22:41:18 -0400

    add proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
      tailwindcss()],
+     server:{
+       proxy:{
+         "/api":{
+           target:"https://voteit.onrender.com/"
+         }
+       }
+     }
 })
